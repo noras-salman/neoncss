46ef5be265792a22cb75da7a273cce06745049df
Noräs Salman <noras@macbook.local>
2023-11-05 22:17:45 -0500

    fix post install

--- postinstall.js
+++ postinstall.js
@@ -1,16 +1,15 @@
 const fs = require("fs");
 
-console.log(fs.readdirSync(`./`));
-fs.rmdirSync("./docs", { recursive: true });
-fs.rmdirSync("./release", { recursive: true });
-fs.rmdirSync("./src", { recursive: true });
+// fs.rmdirSync("./docs", { recursive: true });
+// fs.rmdirSync("./release", { recursive: true });
+// fs.rmdirSync("./src", { recursive: true });
 
-const path = "./";
-let regex = /\.(html|md)/g;
+// const path = "./";
+// let regex = /\.(html|md)/g;
 
-fs.readdirSync(path)
-  .filter((f) => regex.test(f))
-  .map((f) => fs.unlinkSync(path + f));
+// fs.readdirSync(path)
+//   .filter((f) => regex.test(f))
+//   .map((f) => fs.unlinkSync(path + f));
 
-fs.unlinkSync("./server.js");
-fs.unlinkSync("./gulpfile.js");
+// fs.unlinkSync("./server.js");
+// fs.unlinkSync("./gulpfile.js");
